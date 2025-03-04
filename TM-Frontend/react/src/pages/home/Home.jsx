c75a94017d4b9203ea4b4680a09a24454d063b3d
metasploit meta <baretoneee@gmail.com>
2025-03-05 05:19:25 +0530

    basic frontend template and css added

--- TM-Frontend/react/src/pages/home/Home.jsx
+++ TM-Frontend/react/src/pages/home/Home.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./home.module.css";
+import { AuthContext } from "../../context/AuthContext";
 
 const Home = () => {
-    const navigate = useNavigate();
-  return (
-    <div className={styles.container}>
-      <div className={styles.container}>
-         <button onClick={()=> navigate("/userLogin")}>User Login</button>
-         <button onClick={()=> navigate("/adminLogin")}>Admin Login</button>
-      </div>
-      <h2>taskAssigner</h2>
-      <p>Select an Login Option</p>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+//   const {  } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Home
+  const handleSubmit = async () => {
+    e.preventDefault();
+    await login(email, password, false);
+  };
+  return (
+     <>
+     <div className={styles.mainDiv}>
+     <h1>taskAssigner</h1>
+  
+      <div className={styles.container}>
+        {/* <button onClick={()=> navigate("/userLogin")}>User Login</button> */}
+        <h2>User_Login</h2>
+        <form onSubmit={handleSubmit} >
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+       <button onClick={() => navigate("/adminLogin")}>Admin Login</button>
+       </div>
+     </>
+  );
+};
+
+export default Home;
