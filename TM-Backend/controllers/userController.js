10462a52598f16d62b36c0231d191ec4e3f091a3
shivam78775 <148251171+shivam78775@users.noreply.github.com>
2025-03-12 08:08:06 +0530

    role is added in user controller

--- TM-Backend/controllers/userController.js
+++ TM-Backend/controllers/userController.js
@@ -107,6 +107,7 @@ export const verifyUser = async (req, res) => {
           _id: user._id, 
           name: user.name,
           email: user.email,
+          role: user.role
         } });
     } catch (error) {
         res.status(500).json({ message: "Server error" });
